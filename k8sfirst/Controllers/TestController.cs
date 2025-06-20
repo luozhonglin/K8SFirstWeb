070c0f243e62276f7ef60f9766558038de19298e
luozhonglin <363815234@qq.com>
2025-06-20 15:04:24 +0800

    1

---
+++ k8sfirst/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace k8sfirst.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string GetName()
+        {
+            return "luozhonglin";
+        }
+
+        [HttpGet]
+        public string GetName2()
+        {
+            return "luozhonglin2";
+        }
+    }
+}
