7d19c3c1bf0c32e9d5d02af263b5a5cc55646a7e
luozhonglin <363815234@qq.com>
2025-06-26 14:08:57 +0800

    1

--- k8sfirst/Program.cs
+++ k8sfirst/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.WebHost.UseUrls("http://*:8100");
+builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
